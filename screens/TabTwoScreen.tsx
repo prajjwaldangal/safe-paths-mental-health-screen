b7e6f8d871c2dbf15a42bcf711b7e981c68ea855
pdangal <prajjwal.dangal@gmail.com>
2020-07-17 16:03:25 -0600

    timer layout coming along

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,15 +1,43 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import {useState, useEffect} from 'react';
+import { StyleSheet , TouchableOpacity, Dimensions, StatusBar} from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+const screen = Dimensions.get('window');
+
+const formatNumber = number => `0${number}`.slice(-2);
+const getRemaining = (time) => {
+    const mins = Math.floor(time / 60);
+    const secs = time - mins * 60;
+    return { mins: formatNumber(mins), secs: formatNumber(secs) };
+}
+
 export default function TabTwoScreen() {
+  const [remainingSecs, setRemainingSecs] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const { mins, secs } = getRemaining(remainingSecs);
+
+  toggle = () => {
+    setIsActive(!isActive);
+  }
+
+  var reset = () => {
+    setRemainingSecs(0);
+    setIsActive(false);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.timerText}>{`${mins}:${secs}`}</Text>
+      <TouchableOpacity onPress={this.toggle} style={styles.button}>
+          <Text style={styles.buttonText}>{isActive ? 'Pause' : 'Start'}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={this.reset} style={[styles.button, styles.buttonReset]}>
+          <Text style={[styles.buttonText, styles.buttonTextReset]}>Reset</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,8 +45,12 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#07121B',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  buttonTimer: {
+    flex: 1, flexDirection: 'row'
   },
   title: {
     fontSize: 20,
@@ -29,4 +61,29 @@ const styles = StyleSheet.create({
     height: 1,
     width: '80%',
   },
+  buttonText: {
+      fontSize: 45,
+      color: '#B9AAFF'
+  },
+  timerText: {
+      color: '#fff',
+      fontSize: 90,
+      marginBottom: 20
+  },
+  buttonReset: {
+      marginTop: 20,
+      borderColor: "#FF851B"
+  },
+  buttonTextReset: {
+    color: "#FF851B"
+  },
+  button: {
+      borderWidth: 10,
+      borderColor: '#B9AAFF',
+      width: screen.width / 2,
+      height: screen.width / 2,
+      borderRadius: screen.width / 2,
+      alignItems: 'center',
+      justifyContent: 'center'
+  }
 });
