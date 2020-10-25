258a1031ef3c54425ed6efe93ff3546d3d17362e
pdangal <prajjwal.dangal@gmail.com>
2020-10-24 22:50:54 -0600

    response from flask server received

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 // import * as request from "request-promise-native";
 import axios from 'axios';
-import { Platform, StyleSheet, Button, Text, TouchableHighlight,
-  TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View } from 'react-native';
+import { Alert, Platform, StyleSheet, Button, Text, TouchableHighlight,
+  TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback, View,
+  Linking} from 'react-native';
 
 export default class Touchables extends Component {
+  // componentDidMount() {
+  //   console.log('I was triggered during componentDidMount')
+  // }
   _onPressButton() {
-      axios.get(`http://0.0.0.0:5000`)
-      .then(res => {console.log("Request sent from ios client");
-    })
+      // axios.get(`http://0.0.0.0:5000/`)
+      axios.get(`http://10.0.0.116:5000/`)
+      .then(res => {
+        console.log(res.data);
+        rdrtUrl = res.data;
+        const supported = await Linking.canOpenURL(rdrtUrl);
+
+        if (supported) {
+          // Opening the link with some app, if the URL scheme is "http" the web link should be opened
+          // by some browser in the mobile
+          await Linking.openURL(rdrtUrl);
+        } else {
+          Alert.alert(`Don't know how to open this URL: ${rdrtUrl}`);
+        }
+    }).catch(function (error) {
+      console.log(error);
+    });
   }
 
   _onLongPressButton() {
